ce65a07166d87c74949ce7bd928a5c85573e977e
MARCOANS <70296197@continental.edu.pe>
2021-06-16 20:00:52 -0500

    Final Fix

--- app/Http/Controllers/Admin/BlocappController.php
+++ app/Http/Controllers/Admin/BlocappController.php
@@ -13,25 +13,25 @@ class BlocappController extends Controller
     public function index()
     {
         $evaluacions = Quizz::all();
-        return view('admin\blocapp\index1') -> with('evaluacions', $evaluacions);
+        return view('admin.blocapp.index1') -> with('evaluacions', $evaluacions);
     }
     public function results($id)
     {
         $quizz=Quizz::find($id);
         $users = user::all();
-       
+
 
         $direcciones = direcciones::all();
-        return view('admin\blocapp\listaestudiantes',['quizz'=>$quizz])-> with('users', $users, 'direcciones', $direcciones,'quizz',$quizz);                 //cambie index por index1
+        return view('admin.blocapp.listaestudiantes',['quizz'=>$quizz])-> with('users', $users, 'direcciones', $direcciones,'quizz',$quizz);                 //cambie index por index1
     }
 
     public function resultsapps()
     {
         //$quizz=Quizz::find();
         //$users = user::all();
-        
+
 
        // $direcciones = direcciones::all();
-        return view('admin\blocapp\listaapp');                 //cambie index por index1
+        return view('admin.blocapp.listaapp');                 //cambie index por index1
     }
 }
